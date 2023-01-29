e3573976020f96d3c363ad4268e5f151c3d8091e
ParkjunHyukkk <wnsgur5232@naver.com>
2023-01-29 21:23:53 +0900

    week2

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,73 @@
 <template>
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>{{ 원룸들[누른거].title }}</h4>
+      <p>상세페이지 내용임</p>
+      <div class="close-btn" @click="모달창열렸니 = false">닫기</div>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="test in 메뉴들" :key="test">{{ test }}</a>
   </div>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <!--<img alt="Vue logo" src="./assets/logo.png" />-->
   <!-- 개선 전 -->
-
+  <!--
   <div>
-    {{ logo }}
-    <h4 class="red" :style="스타일">{{ products[0] }}</h4>
-    <p>50 만원</p>
+    <img :src="원룸들[0].image" class="room-img" />
+    <h4 @click="모달창열렸니 = true" class="red" :style="스타일">
+      {{ 원룸들[0].title }}
+    </h4>
+    <p>{{ 원룸들[0].price }} 원</p>
     <button @click="increase1">허위매물신고</button>
     <span>신고수 : {{ 신고수[0] }}</span>
   </div>
+
   <div>
+    <img src="./assets/room1.jpg" class="room-img" />
     <h4 class="red" :style="스타일">{{ products[1] }}</h4>
     <button @click="increase2">허위매물신고</button>
     <span>신고수 : {{ 신고수[1] }}</span>
   </div>
+
   <div>
+    <img src="./assets/room2.jpg" class="room-img" />
     <h4 class="red" :style="스타일">{{ products[2] }}</h4>
     <button @click="increase3">허위매물신고</button>
     <span>신고수 : {{ 신고수[2] }}</span>
   </div>
-
+-->
   <!-- 개선 후-->
 
-  <!-- {{ logo }}
-  <div v-for="(a, i) in products" :key="a">
-    {{ products[i] }}
-    <p>50</p>
-  </div> -->
-
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div v-for="(a, i) in 원룸들" :key="i">
+    <img :src="원룸들[i].image" class="room-img" />
+    <h4
+      @click="
+        모달창열렸니 = true;
+        누른거 = i;
+      "
+      :style="스타일"
+      i
+      class="red"
+    >
+      {{ 원룸들[i].title }}
+    </h4>
+    <p>{{ 원룸들[i].price }} 원</p>
+    <p>{{ 원룸들[i].content }} 원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-// 함수 사용법
+//import HelloWorld from "./components/HelloWorld.vue";
+import data from "./assets/data";
+// 함수 메소드 추가 후 사용법
 export default {
   name: "App",
   data() {
     return {
+      누른거: 0,
+      원룸들: data,
+      모달창열렸니: false, // 데이터 먼저
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
       스타일: "color:orange",
       메뉴들: ["HOME", "SHOP", "ABOUT"],
@@ -59,12 +86,47 @@ export default {
     },
   },
   components: {
-    HelloWorld,
+    //HelloWorld,
   },
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.close-btn {
+  width: 10%;
+  background: purple;
+  padding: 20px;
+  text-align: center;
+  margin-left: 45%;
+  border-radius: 8px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -82,5 +144,10 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
